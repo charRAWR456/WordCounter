193b14a2fa3549930ec42ac06ffd855e7bbbc9ba
Jamie Pittak <charrawr456@gmail.com>
2018-02-19 13:01:17 -0800

    add home view test

--- WordCounter.Tests/ControllerTests/HomeControllerTest.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTest.cs
@@ -9,18 +9,18 @@ namespace WordCounter.Tests
   [TestClass]
   public class HomeControllerTest
   {
-    // [TestMethod]
-    // public void Index_ReturnsHomeView_True()
-    // {
-    //   //Arrange
-    //   HomeController controller = new HomeController();
-    //
-    //   //Act
-    //   IActionResult homeView = controller.Home();
-    //   ViewResult result = homeView as ViewResult;
-    //
-    //   //Assert
-    //   Assert.IsInstanceOfType(result, typeof(ViewResult));
-    // }
+    [TestMethod]
+    public void Index_ReturnsHomeView_True()
+    {
+      //Arrange
+      HomeController controller = new HomeController();
+
+      //Act
+      IActionResult homeView = controller.Home();
+      ViewResult result = homeView as ViewResult;
+
+      //Assert
+      Assert.IsInstanceOfType(result, typeof(ViewResult));
+    }
   }
 }
