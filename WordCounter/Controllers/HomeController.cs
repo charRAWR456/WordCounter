4857cf65f9dd40e5e7e48dcf00c502491434ab71
Jamie Pittak <charrawr456@gmail.com>
2018-02-19 13:48:33 -0800

    add JS to get values from form

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -6,10 +6,24 @@ namespace WordCounter.Controllers
 {
   public class HomeController : Controller
   {
+    private WordCount _myWordCount = new WordCount();
+
     [HttpGet("/home")]
     public ActionResult Home()
     {
       return View();
+    }
+    [HttpPost("/result")]
+    public ActionResult SubmitResult()
+    {
+    if (_myWordCount.FieldCheck(Request.Form["user-word"], Request.Form["user-sentence"]) == "Please enter a word AND sentence")
+    {
+      string result = "Please enter a word AND sentence";
+      return View("Result", result);
+    }else{
+      int result = _myWordCount.RepeatCounter(Request.Form["user-word"], Request.Form["user-sentence"]);
+      return View("Result", result);
+    }
     }
   }
 }
---
+++ WordCounter/wwwroot/js/scripts.js
@@ -0,0 +1,5 @@
+$(function(){
+  $(".evaluate").click(function(){
+  var  _sentence = $("#user-sentence").val();
+  var _word = $("#user-word").val();
+  });
